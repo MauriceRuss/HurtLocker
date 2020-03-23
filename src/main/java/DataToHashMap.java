c351db1e31bc3c8327145f4b898eadcf237e7f25
Mo Russ <mauriceruss302@gmail.com>
2020-03-23 13:50:51 -0400

    made DataToHashMap class. trying to turn datafile to hashmap to work out key value issues. still try to work out method to do this. brain hurts. no love for tyriq

---
+++ src/main/java/DataToHashMap.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class DataToHashMap {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
