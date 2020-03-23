9be5cfe278823d3a03fe46f021f131b75a494c6f
Mo Russ <mauriceruss302@gmail.com>
2020-03-23 12:09:40 -0400

    parseMilk(), parseBread(), parseCookies(), and parseAplles() made

--- src/main/java/JerkSON.java
+++ src/main/java/JerkSON.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JerkSON {
     // split raw data array to strings
     // make correct key value pairs by a :
@@ -14,4 +17,37 @@ public class JerkSON {
         String[] parsedList = main.readRawDataToString().split("##");
         return parsedList;
     }
+
+
+    public String parseMilk(String name)throws Exception{
+        Pattern milk = Pattern.compile("[Mm][Ii][Ll][Kk]");
+        Matcher correctMilkName = milk.matcher(name);
+        correctMilkName.find();
+        return correctMilkName.replaceAll("Milk");
+    }
+
+    public String parseBread(String name)throws Exception{
+        Pattern bread = Pattern.compile("[Bb][Rr][Ee][Aa][Dd]");
+        Matcher correctBreadName = bread.matcher(name);
+        correctBreadName.find();
+        return correctBreadName.replaceAll("Milk");
+    }
+
+    public String parseCookies(String name)throws Exception{
+        Pattern cookies = Pattern.compile("[Cc][Oo0][Oo0][Kk][Ii][Ee][Ss]");
+        Matcher correctCookiesName = cookies.matcher(name);
+        correctCookiesName.find();
+        return correctCookiesName.replaceAll("Milk");
+    }
+
+    public String parseApples(String name)throws Exception{
+        Pattern apples = Pattern.compile("[Cc][Oo0][Oo0][Kk][Ii][Ee][Ss]");
+        Matcher correctApplesName = apples.matcher(name);
+        correctApplesName.find();
+        return correctApplesName.replaceAll("Milk");
+    }
+
+
+
+
 }
