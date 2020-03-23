e9ddedceef7e2bd3dffa9b5715a83717cddc986e
Mo Russ <mauriceruss302@gmail.com>
2020-03-23 10:59:16 -0400

    findStringParse()

--- src/main/java/JerkSON.java
+++ src/main/java/JerkSON.java
@@ -1,2 +1,17 @@
 public class JerkSON {
+    // split raw data array to strings
+    // make correct key value pairs by a :
+    //correct case sensativity of data upper case first char lower rest
+    //gaurantee every key has a value and every value has a pair
+    Main main = new Main();
+
+    /**split raw data array to strings
+     *
+     * @return an array of strings
+     * @throws Exception
+     */
+    public String[] findStringParse() throws Exception{
+        String[] parsedList = main.readRawDataToString().split("##");
+        return parsedList;
+    }
 }
